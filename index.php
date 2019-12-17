d0f5756fb52d9520bb890679d7704c0918f417b6
prashant-ux <58131800+prashant-ux@users.noreply.github.com>
2019-12-17 17:20:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <title>inline styles</title>
 </head>
 <body>
+  <body bgcolor="yellow">
 <h1 style="color:blue;size:50;text-align:center;background-color;yellow">||Thank  you  so  much  for  the  wishes||</h1>
 <h2 style="color:green;size:100;text-align:center;">*CHHOTU*</h2>
 </body>
